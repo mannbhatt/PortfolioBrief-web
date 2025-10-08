6b2526bfcc13eb14d6931011b1e3a5ec8ca2b1b0
Mann <manb35624@gmail.com>
2025-10-09 01:52:47 +0530

    port varable declare

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
-process.env.PORT || 5000;
+const port=process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(cors({
